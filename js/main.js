759d8fa0be80bbb62184aeb39f484c8da59303f6
Bekmirzo <99716618+Bekmirzo@users.noreply.github.com>
2022-06-20 01:34:10 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,144 @@
+const nq = [
+    {
+        id: 0,
+        Nomi: `Tarixi Mullozoda (Kitobi Mullozoda).`,
+        Muallif: `Ahmad ibn Mahmud Buxoriy (Muʼinul-fuqaro)`,
+        Tili: `forsiy.`,
+        Hajmi: `134-sahifa. `,
+        Xattot: `Odinaxoja Qorakoʻli. Yaxshi saqlangan.`,
+        Qulyozma: `1244/y1828-yil koʻchirilgan.`,
+        Yozuvi  : `nastaʼliq. Hajmi: 13x20sm.`,
+        Muqovasi: `qora “mugʻziy” uslub, 3-tadan tamgʻa mavjud.`,
+        img: [
+            './img/0/0.jpg',
+        ],
+        text: `Asar muallifi XVI asrda yashagan Ahmad ibn Mahmud – “Muʼinul-fuqaro” boʻlib, Buxoro shahri va atrofu aknofidagi mashhur mozorotlar va muqaddas qadamjolar tarixi haqida soʻz yuritadi.`
+    },
+    {
+        id: 1,
+        Nomi: `Qurʼoni Karim`,
+        Muallif: `Alloh tomonidan yuborilgan kitob`,
+        Tili: `Arab tili.`,
+        Hajmi: `321 varaq`,
+        Xattot: `Xattoti mavjud emas. Ammo, mutaxassislar yozuv uslubiga qarab Buxorolik xattot Abdullobek tomonidan koʻchirilgan deb xulosa beradilar`,
+        Qulyozma: `1304h/1887yil koʻchirilgan`,
+        Yozuvi  : `Nasx xatida`,
+        Muqovasi: `Muqovasi pishiq kartondan tayyorlangan boʻlib, toʻq qizil rangda. Ikki tomon muqovada bittadan ikkita katta va ikkitadan toʻrtta kichik kartushi (turunji) mavjud boʻlib, unda ushbu kitobni muqovalagan sahhofning nomi yozilgan. Qori Ibodulloh ibn Mirodil xoʻja.`,
+        img: [
+            './img/1/1.jpg',
+            './img/1/2.jpg',
+            './img/1/3.jpg',
+            './img/1/4.jpg',
+            './img/1/5.jpg',
+            './img/1/6.jpg',
+        ],
+        text: `Asarni koʻchirgan shaxs Buxorolik yetuk xattot Domla Bobobekning shogirdlaridan hisoblanadi. Kitob oyat nozil qilingan davrdan to qiyomatga qadar insonlarni ezgulikka boshlab, yomonlikdan qaytaruvchi mukammal moʻtabar asar sifatida asrab avaylab kelinmoqda.
+        Ushbu kitob juda bejirim, bosh unvon qismida juda goʻzal suratda tillo suvi bilan zeb berib koʻchirilgan. Keyingi varaqlarida ham garchi birinchi varagʻidagichalik boʻlmasada tillo suvi bilan ramkalar chizilgan. Albatta bunday qoʻlyozmalar martabali, mansabdor kishilarga buyurtma asosida koʻchirib berilgan nusxalar hisoblanadi. Bunday qoʻlyozmalarni koʻchirishda juda koʻp mablagʻ sarflanishi sababli oddiy xalq bunday qoʻlyozmalarni olib oʻqiyolmagan
+        `
+    },
+    {
+        id: 2,
+        Nomi: `“Fatavoi mustasfo” (Sof fatvolar)`,
+        Muallif: `Abul Barakot Abdulloh bin Ahmad an-Nasafiy.`,
+        Tili: `Arab tili.`,
+        Hajmi: `228 varaq`,
+        Xattot: `Koʻchiruvchisi mavjud emas. Ammo, ushbu kitob muallif hayotlik davrida koʻchirilgan asarlardan sanalgani uchun ushbu kitobni muallif tomonidan koʻchirilgan avtograf qoʻlyozma deb mutaxassis manbashunoslar eʼtirof etadilar`,
+        Qulyozma: `685h/1286yil koʻchirilgan`,
+        Yozuvi  : `Nasx xatida`,
+        Muqovasi: `muqovasi kartondan tayyorlangan boʻlib, qoramtir, jigar rangda. `,
+        img: [
+            './img/2/0.jpg',
+            './img/2/1.jpg',
+            './img/2/2.jpg',
+            './img/2/3.jpg',
+        ],
+        text: `Ushbu asar islom huquqshunosligiga oid manba hisoblanadi. Ushbu asarga Musaffo fatvolar yaʼni pokiza fatvolar deb nom berilishiga sabab balkim bu kitobda keltirilgan fatvolar Shamsul aimma Halvoiy, Shamsul aimma Saraxsiy va boshqa ulugʻ allomalarning moʻtabar asarlaridan tanlab olingan fatvolar boʻlganligi uchun ham shunday nom berilgan boʻlishi ham mumkin. Ilmiy izlanish asnosida bu asarning bir necha joyida Muhammad bin Mahmud al-Hofiz al-Buxoriy (Xoʻja Porso) ning shaxsiy muhrlari bosilganligini guvohi boʻldik. Bundan shu narsa maʼlum boʻladiki, mazkur asar moʻtabar manba sifatida Xoʻja Porsoning shaxsiy kutubxonalaridan joy olgan boʻlishi ehtimoldan xolidir.
+        `
+    },
+    {
+        id: 3,
+        Nomi: `Mavohibi Aliyya yoki boshqacha nomi Tafsiri Husayniy.`,
+        Muallif: `Husayn Voiz Koshifiy.`,
+        Tili: `Arab-fors tilida.`,
+        Hajmi: `647 varaq`,
+        Xattot: `Mutaxassislar tomonidan muallif qalamiga mansub deb eʼtirof etiladi.`,
+        Qulyozma: `Koʻchirilgan sanasi koʻrsatilmagan. Ammo, mutaxassislar qogʻozi, kitobdagi yozuv uslubi va undagi bezash usullaridan kelib chiqib hirot maktabiga oid qoʻlyozma boʻlib, taxminan XV-XVI asrda muallif oʻz qoʻli bilan koʻchirgan yaʼni avtograf qoʻlyozmalardan sanaladi deb eʼtirof etganlar.   `,
+        Yozuvi  : `Nasx, nastaʼliq xatida koʻchirilgan  `,
+        Muqovasi: `Kartondan ishlangan boʻlib, jigar rangda. Muqovada boshqa kitoblarda koʻp ham koʻzga tashlanmaydigan “Labaki” si ham mavjud.`,
+        img: [
+            './img/3/0.jpg',
+            './img/3/1.jpg',
+            './img/3/2.jpg',
+            './img/3/3.jpg',
+            './img/3/4.jpg',
+        ],
+        text: `Asar muallifi Alisher Navoiy davrida yashab ijod etgan alloma sifatida tarixda maʼlum.
+        Kitobning avalgi varagʻlarida juda bejirim Hirot maktabiga xos bezaklar bilan bezatilgan unvoni mavjud. Asosiy matnlar (Qurʼon oyatlari qismi) arab tilida boʻlib nasx xatida, tafsir-sharh qismi esa fors tilida boʻlib, nastaʼliq elementlari ham koʻzga tashlanadi. Kitobda Qurʼoni karimning 114 ta surasi va uning forscha sharhi ifodalangan.
+   
+        `
+    }
+];
+
+const nqselect = document.getElementById('nqselect');
+
+function renderList(){
+    nqselect.innerHTML = '';
+    nq.forEach(element => {
+        let item = document.createElement('option');
+        item.value = element.id;
+        item.innerHTML = element.Nomi;
+        nqselect.appendChild(item);
+    });
+    nqselect.firstElementChild.setAttribute('selected', true);
+};
+
+renderList();
+
+const nomi = document.getElementById('nomi');
+const muallif = document.getElementById('muallif');
+const tili = document.getElementById('tili');
+const hajmi = document.getElementById('hajmi');
+const xattot = document.getElementById('xattot');
+const qulyozma = document.getElementById('qulyozma');
+const yozuvi = document.getElementById('yozuvi');
+const muqovasi = document.getElementById('muqovasi');
+const text = document.getElementById('text');
+const cardImgDiv1 = document.getElementById('cardImgDiv1');
+const cardImgDiv2 = document.getElementById('cardImgDiv2');
+
+nqselect.onchange = (e) => {
+    let itemId = e.target.value;
+    let images = nq[itemId].img;
+
+    cardImgDiv1.innerHTML = '';
+    cardImgDiv2.innerHTML = '';
+    for(let i = 0; i < images.length; i++){
+        let cardButton = document.createElement('button');
+        cardButton.setAttribute('type', 'button');
+        cardButton.setAttribute('data-bs-target', '#carouselExampleIndicators');
+        cardButton.setAttribute('data-bs-slide-to', i);
+        cardButton.setAttribute('aria-label', `Slide ${i+1}`);
+        cardImgDiv1.appendChild(cardButton);
+
+        let cardImg = document.createElement('div');
+        cardImg.className = 'carousel-item';
+        cardImg.innerHTML = `<img src="${images[i]}" class="cimg" alt="...">`
+        cardImgDiv2.appendChild(cardImg);
+    }
+    cardImgDiv1.firstElementChild.className = 'active';
+    cardImgDiv1.firstElementChild.setAttribute('aria-current',true);
+    cardImgDiv2.firstElementChild.classList.add('active');
+
+
+    nomi.innerHTML = nq[itemId].Nomi;
+    muallif.innerHTML = nq[itemId].Muallif;
+    tili.innerHTML = nq[itemId].Tili;
+    hajmi.innerHTML = nq[itemId].Hajmi;
+    xattot.innerHTML = nq[itemId].Xattot;
+    qulyozma.innerHTML = nq[itemId].Qulyozma;
+    yozuvi.innerHTML = nq[itemId].Yozuvi;
+    muqovasi.innerHTML = nq[itemId].Muqovasi;
+    text.innerHTML = nq[itemId].text;
+
+
+};
